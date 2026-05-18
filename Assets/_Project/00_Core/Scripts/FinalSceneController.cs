6c6b8707d81b02318bdf247a0774fe6dcaa20fad
jehnmy <jenhmy@gmail.com>
2026-05-18 22:23:54 +0200

    arreglado g2 boton y transicions texto mejor ubicado

--- Assets/_Project/00_Core/Scripts/FinalSceneController.cs
+++ Assets/_Project/00_Core/Scripts/FinalSceneController.cs
@@ -28,23 +28,23 @@ public class FinalSceneController : MonoBehaviour
     {
         if (puntos < 1000)
         {
-            return "¡Superviviente galáctico!\nHas llegado al final, aunque con más harina en la cara que puntos en el marcador.";
+            return "¡SUPERVIVIENTE GALÁCTICO!\n\nHas llegado al final, aunque con más harina en la cara que puntos en el marcador.";
         }
         else if (puntos < 2000)
         {
-            return "¡Explorador galáctico!\nNo todos los héroes llevan capa; algunos llevan rodillo y harina.";
+            return "¡EXPLORADOR GALÁCTICO!\n\nNo todos los héroes llevan capa; algunos llevan rodillo y harina.";
         }
         else if (puntos < 3000)
         {
-            return "¡Guardián galáctico!\nLa galaxia duerme tranquila sabiendo que tu horno sigue encendido.";
+            return "¡GUARDIÁN GALÁCTICO!\n\nLa galaxia duerme tranquila sabiendo que tu horno sigue encendido.";
         }
         else if (puntos < 4000)
         {
-            return "¡Capitán galáctico!\nHas dejado la galaxia calentita, crujiente y bastante libre de alienígenas.";
+            return "¡HÉROE GALÁCTICO!\n\nHas dejado la galaxia calentita, crujiente y bastante libre de alienígenas.";
         }
         else
         {
-            return "¡Leyenda galáctica!\nLos aliens han firmado la paz solo para conseguir tu receta de galletas.";
+            return "¡LEYENDA GALÁCTICA!\n\nLos aliens han firmado la paz solo para conseguir tu receta de pan.";
         }
     }
 }
